843a1dbef90f5891da8aaf30dbe621bb2c6ea80a
Sandeep Konakanchi <sxk80270@ucmo.edu>
2018-07-14 21:41:07 -0400

    Vue modal1

---
+++ modal1.js
@@ -0,0 +1,21 @@
+Vue.component('modal', {
+    template: 
+    `
+    <div class="modal is-active">
+        <div class="modal-background"></div>
+        <div class="modal-content">
+            <div class="box">
+                <p>Hey I'm sandeep from VueJs</p>
+            </div>
+        </div>
+        <button class="modal-close is-large" aria-label="close" @click="$emit('close')"></button>
+    </div>
+    `
+});
+
+new Vue({
+    el: "#root",
+    data: {
+        showModal: false
+    }
+});
