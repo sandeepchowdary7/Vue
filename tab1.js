2e4b7d4fc437e1a13239e71a1feea3f4b54b8563
Sandeep Konakanchi <sxk80270@ucmo.edu>
2018-07-15 22:44:58 -0400

    Vue-tab inprogress-need to work

---
+++ tab1.js
@@ -0,0 +1,41 @@
+Vue.component('tabs', {
+
+    template: 
+        `
+        <div>
+            <div class="tabs">
+                <ul>
+                    <li class="is-active"><a>Pictures</a></li>
+                    <li><a>Music</a></li>
+                    <li><a>Videos</a></li>
+                    <li><a>Documents</a></li>
+                </ul>
+            </div>
+
+            <div class="tabs-details">
+                <slot></slot>
+            </div>
+        </div>
+        `,
+
+        mounted() {
+            console.log(this.$children);
+        }
+});
+
+Vue.component('tab', {
+    template: 
+    `
+    <div><slot></slot></div>
+    `,
+
+    props: {
+        name: {required:true}
+    }
+});
+
+
+
+new Vue({
+    el: "#app"
+})
