ef0d95ff2c78de4d3adfa5b6d7735135b04b55a2
Sandeep Konakanchi <sxk80270@ucmo.edu>
2018-07-22 18:37:17 -0400

    Vue Tab
    Fixed Vue tab

--- tab1.js
+++ tab1.js
@@ -5,11 +5,10 @@ Vue.component('tabs', {
         <div>
             <div class="tabs">
                 <ul>
-                    <li class="is-active"><a>Pictures</a></li>
-                    <li><a>Music</a></li>
-                    <li><a>Videos</a></li>
-                    <li><a>Documents</a></li>
-                </ul>
+                    <li v-for="tab in tabs" :class="{ 'is-active': tab.isActive }">
+                        <a :href="tab.href" @click='selectTab(tab)'>{{ tab.name  }}</a>
+                    </li>
+                  </ul>
             </div>
 
             <div class="tabs-details">
@@ -18,17 +17,46 @@ Vue.component('tabs', {
         </div>
         `,
 
-        mounted() {
-            console.log(this.$children);
+        data(){
+            return{ tabs:  [] };
+        },
+
+        created() {
+           this.tabs = this.$children;
+        },
+
+        methods: {
+            selectTab(selectedTab){
+                this.tabs.forEach(tab => {
+                     tab.isActive = (tab.name == selectedTab.name); 
+                })
+            }
         }
 });
 
 Vue.component('tab', {
     template:
-    `<div><slot></slot></div> `,
+    `<div v-show=isActive><slot></slot></div> `,
 
     props: {
-        name: {required:true}
+        name: { required: true },
+        selected: { default: false }
+    },
+
+    data(){
+       return {
+              isActive: false
+       } 
+    },
+
+    computed: {
+        href() {
+                return '#' + this.name.toLowerCase(). replace(/ /g, '-');
+        }
+    },
+
+    mounted () {
+        this.isActive = this.selected;
     }
 });
 
