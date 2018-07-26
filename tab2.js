463662e743e1f1bc8aa5a046430393875d415638
Sandeep Konakanchi <sxk80270@ucmo.edu>
2018-07-26 00:39:39 -0400

    Vue-tab2
    Fixed issues with vue-tab2

--- tab2.js
+++ tab2.js
@@ -5,8 +5,8 @@ Vue.component('tabs', {
 
         <div class="tabs">
             <ul>
-                <li v-for="tab in tabs">
-                <a href="#">  {{ tab.name }} </a>
+                <li v-for="tab in tabs" :class="{'is-active': tab.isActive}">
+                <a :href="tab.href" @click="selectTab(tab)">  {{ tab.name }} </a>
                 </li>
             </ul>
         </div>
@@ -24,19 +24,44 @@ Vue.component('tabs', {
 
     created() {
         this.tabs = this.$children;
+    },
+
+    methods: {
+        selectTab(selectedTab) {
+         this.tabs.forEach(tab  => {
+             tab.isActive  = (tab.name == selectedTab.name);
+            })
+         }
     }
 });
 
 Vue.component('tab', {
   template:
     `
-    <div><slot></slot></div>
+    <div v-show="isActive"><slot></slot></div>
     `,
 
     props: {
-        name: {required: true}
+        name: {required: true},
+        selected: {default: false}
+    },
+
+    data(){
+        return {
+            isActive: false
+        }
+    },
+
+    computed: {
+        href() {
+            return '#' + this.name.toLowerCase().replace(/ /g, '-');
+        }
+    },
+
+    mounted () {
+         this.isActive = this.selectTab;
     }
-});
+}),
 
 new Vue({
     el: '#tab2',
