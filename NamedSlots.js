246a0d94ab271ec6d7a27d1433d63e50698da399
Sandeep Konakanchi <sxk80270@ucmo.edu>
2018-07-29 12:04:32 -0400

    Namedslots-Vue
    Name slots in Vue.js

--- NamedSlots.js
+++ NamedSlots.js
@@ -1,25 +1,30 @@
 Vue.component('modal', {
     template: 
     `
-        <div class="modal">
-            <div class="modal-background"></div>
-            <div class="modal-card">
-                <header class="modal-card-head">
-                <p class="modal-card-title">Modal title</p>
-                <button class="delete" aria-label="close"></button>
-                </header>
-                <section class="modal-card-body">
-                <!-- Content ... -->
-                </section>
-                <footer class="modal-card-foot">
-                <button class="button is-success">Save changes</button>
-                <button class="button">Cancel</button>
-                </footer>
-            </div>
+     <div class="modal is-active">
+        <div class="modal-background"></div>
+
+        <div class="modal-card">
+
+        <header class="modal-card-head">
+            <p class="modal-card-title">Modal title</p>
+            <slot name="header"></slot>
+            <button class="delete" aria-label="close"></button>
+        </header>
+
+        <section class="modal-card-body">
+                 <slot></slot>
+        </section>
+
+        <footer class="modal-card-foot">
+            <slot name="footer"></slot>
+        </footer>
+
+        </div>
     </div>
     `
-
 });
+
 new Vue({
     el: "#slot"
 })
